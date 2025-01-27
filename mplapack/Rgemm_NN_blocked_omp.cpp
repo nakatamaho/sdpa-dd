593985e970b2b5146c9027c90834d8f91ae093ba
NAKATA Maho <maho.nakata@gmail.com>
2025-01-27 14:13:18 +0900

    .

--- mplapack/Rgemm_NN_blocked_omp.cpp
+++ mplapack/Rgemm_NN_blocked_omp.cpp
@@ -53,7 +53,6 @@ static inline void Rgemm_block_4x4_kernel(const dd_real (&A_block)[4][4], const 
 }
 
 void Rgemm_NN_blocked_omp(mplapackint m, mplapackint n, mplapackint k, dd_real alpha, dd_real *A, mplapackint lda, dd_real *B, mplapackint ldb, dd_real beta, dd_real *C, mplapackint ldc) {
-
     if (m % 4 != 0 || n % 4 != 0 || k % 4 != 0) {
         std::cerr << "Error: Matrix dimensions must be multiples of 4" << std::endl;
         exit(1);
@@ -82,22 +81,18 @@ void Rgemm_NN_blocked_omp(mplapackint m, mplapackint n, mplapackint k, dd_real a
     for (mplapackint j0 = 0; j0 < n; j0 += 4) {
         for (mplapackint i0 = 0; i0 < m; i0 += 4) {
             dd_real C_block[4][4] = {{0}};
-
             for (mplapackint k0 = 0; k0 < k; k0 += 4) {
                 dd_real A_block[4][4], B_block[4][4];
-
                 for (mplapackint ii = 0; ii < 4; ++ii) {
                     for (mplapackint kk = 0; kk < 4; ++kk) {
                         A_block[ii][kk] = A[(i0 + ii) + (k0 + kk) * lda];
                     }
                 }
-
                 for (mplapackint kk = 0; kk < 4; ++kk) {
                     for (mplapackint jj = 0; jj < 4; ++jj) {
                         B_block[kk][jj] = B[(k0 + kk) + (j0 + jj) * ldb];
                     }
                 }
-
                 Rgemm_block_4x4_kernel(A_block, B_block, C_block);
             }
             for (mplapackint ii = 0; ii < 4; ++ii) {
